41763bcd5dcff3652b11bca3e60bb64072dcc1af
Sebastian Bürgel <s.buergel@gmail.com>
2015-12-20 00:27:29 +0100

    custom contracts can be used for name registry and content registry. issue: ipfs hashes are too long for current 32-byte solidity strings, byte arrays do not seems to work.

--- dlabbook.js
+++ dlabbook.js
@@ -1,10 +1,39 @@
 $(function(){
-    nameRegContractAddress = "0x06b179aabf198ced0f98c8ceca905a920a137ef4";
-    contRegContractAddress = "0x17956ba5f4291844bc25aedb27e69bc11b5bda39";
-    defaultAccount = "0xcd2a3d9f938e13cd947ec05abc7fe734df8dd826";
+	//Cookies.set('address', 'Hello Cookie');
+	//console.log(Cookies.get('address'));
+	// todo
+	//	figure out function definition / work flow at interface of JS and solidity:
+	/*		- find out if current geth address is registered, if so use it and display "registered as [name]" in header
+			- if no address is registered, send user first to "register" page
+			- make seletion of all available accounts in settings as table:
+			
+			ethereum address		registered to			funds
+			0xasdlkfrkj4rhf  		s.buergel@gmail.com		10 ether
+			0x324o324r09239vf		sbuergel@ethz.ch		123 szabo
+			0x234kjh123kjhds		[register now]			1 ether
+			[create new address]
+			
+			- on publish: 
+				1. send to IPFS (done)
+				2. send ipfs hash from currently selected address to contract
+	*/
 	
+	//	- allow user to paste abi + evm code and deploy contract from that (in case no compiler is available)
+	//		implementation: 
+	//		- add button "add code manually" next to "compile" button, this shows two text boxes for abi + code where user can paste data
+	//		- abi + code from normal compile should get auto-filled into text box as well so that user could manipulate abi or byte-code manually for experimenting
+	//		- when one set of text boxes is shown, a second button appears "add contract"
+	//	- contracts for name registry and content registry should be user-settable in settings tab
+	//		- each contract that is deployed should have a button "use as name reg" and "use as content reg" which
+	//		  which is updating the address, and abi definition in the settings tab
+	//		- function which is called on "register user", "find accounts" or "publish content" should be user-selectable
+	//	- make all JS + HTML code editable and deployable to IPFS (and using this notary service)
+	//		- copy text to some editor
+	//		- find out how to handle separate files (web3.js, ipfs.js, ...)
+		
 	contracts = [];
-	/* 
+
+/* 
 //	buddies: (for approving change of address)
 //		B1 adds address of buddies B2, B3, B4 and number N to the contract D1
 //		if N of the buddies B2, ... BN approve, 
@@ -18,22 +47,23 @@ $(function(){
 //		unregister account (needs approval of owner or N buddies)
 		
 	on startup:
-		check availability of IPFS (try publishing)
+#		check availability of IPFS (try publishing)
 		check availability of geth (check if we have access to an unlocked account with some Ether)
 		
 	on publish:
 		find username of address
 //		encrypt content
-		publish IPFS hash
-		register IPFS hash as content of address
+#		publish IPFS hash
+#		register IPFS hash as content of address
 		
 	publishing priorities
 		connect to local geth and publish to pre-coded address and abi
 		fallback 1: connect to publicly available geth (which is later exposed via API, not giving everyone full access)
 		alternative 1: connect to some other geth node
-//		alternative 2: call / deploy custom contract 
+#		alternative 2: call / deploy custom contract 
 		
 	*/
+
     // click on menu items
     $("#navPublish").click(function(event){
         $("#contentBrowse").hide();
@@ -114,19 +144,11 @@ $(function(){
         //var contract = web3.eth.contract(abi);
         //instance = contract.at(address);
 
-
-        // TODO: let user user choose to connect to which web3 provider to connect (sandbox or (local) other)
-		// have "advanced" settings which can be opened and contain the solidity source code.
+        // TODO: 
 		// try to compile using the local solidity compiler. if this fails use byte code (which is also supplied via "advanced" settings)
 		// if contract cannot be deployed (because, e.g. geth or the solidity compiler is not available),
-		//      connect to to pre-shared contract address 
+		//      connect to to pre-shared contract address + abi
 		//      (warning: this will only work in the main net and the sandbox if we deployed that contract on sandbox start-up)
-		//      TODO (Andre): check how we can setup our own test net so we can kind of easily test
-		//       |--> Raspberry Pi with IPFS+GETH setup --> done
-		//                                      |--> couple bits still to do (DNS/Firewall/etc) --> open
-		//                                      |--> divert traffic from IDE to external server --> open
-		//                      |--> we can connect to the sandbox from remote therefore this shouldnt be necessary (for the hackaton)
-		//                          but might be a nice test-setup in the long run
 		
 		if (!web3.isConnected())
 		{
@@ -169,41 +191,45 @@ $(function(){
         var username = $("#username").val();
         if (username.length < 5)
         {
-            $(".container").prepend("<div class='alert alert-danger alert-dismissible' role='alert'role='alert'><button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button><strong>Error!</strong>Username has to be at least 5 characters long.</div>");
+            error("username has to be at least 5 characters long");
         }
         else
         {
-			// Ethereum currently not available (for debugging purposes)
-            /*// check if username is already registered
+            // check if username is already registered
             if (!instanceNameReg.IsRegistered(username))
             {
-                $(".container").prepend("<div class='alert alert-info alert-dismissible role='alert'role='alert'><button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button><strong>Info:</strong>User " + username + " has not been registered yet, we will do that now.</div>");
+                info("user " + username + " has not been registered yet, we will do that now.");
                 instanceNameReg.Register(username);
             }
             else
-                $(".container").prepend("<div class='alert alert-success alert-dismissible role='alert'role='alert'><button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button><strong>Info:</strong>You are registered as " + username + ".</div>");
-			*/
-            $("#debug").append("<br />");
-            $("#debug").append($("#dataToPublish").val());
-            
+                info("you are registered as " + username);
+			try
+			{
+            info("publishing: " + $("#dataToPublish").val());
             // publish data to IPFS
         	updateIpfsAddress();
         	var buf = new ipfs.Buffer($("#dataToPublish").val());
         	ipfs.add(buf, function(err, res){
         	    // note: this function is executed asynchronously
         	    // i.e. the content of hash will most likely not be available yet outside of this function (synchronous code)
-                if (err || !res) return console.log(err)
+                if (err || !res) 
+					return console.log(err);
         	    var hash = res.Hash;
             	$("#ipfsPublishInfo").html("IPFS address (hash) of the data: <a href='https://gateway.ipfs.io/ipfs/" + hash + "'>" + hash + "</a>");
             	$("#ipfsPublishInfo").fadeIn(1000);
             	
             	// publish to Ethereum
-            	//instanceContReg.registerContent(hash);
-        	});
+            	instanceContReg.registerContent(hash);
+        	});	
+			}
+			catch (e)
+			{
+				error (e);
+			}
         }
     });
     
-	$("#updateGethAddress").click(function(event){
+	$("#updateGethSettings").click(function(event){
 		updateGethAddress();
 	});
 	$("#updateIpfsAddress").click(function(event){
@@ -227,6 +253,7 @@ $(function(){
 			// remove all content of previous compile runs
 			$("#contractContent").empty();	// remove from UI
 			contracts = [];					// remove from compile output
+
 			var c = 0;
 			for (var contractName in compiled)
 			{	// looping over all contracts (we might have more than one contract in code base)
@@ -238,27 +265,48 @@ $(function(){
 				source = contractJson.info.source;
 				
 				// appending compiled output to contracts array
+				// todo: we might not need to store abi and evmCode here if we use the text boxes during deployment
 				contracts.push(new contrHolder(source, abi, evmCode));
 				contracts[c].status = 1;
 
 				// todo: should abi be escaped to prevent some html injections? (is this even possible from JSON?)
 				
+				// display compiled output of contract
 				$("#contractContent").append(
 				"<div class=\"row\"><div class=\"col-md-12\"><h3>contract " + 
 				contractName + 
 				"</h3></div></div>" + 
-				"<div class=\"row\"><div class=\"col-md-4\">abi: </div>" + 
-				"<div class=\"col-md-8\" style=\"word-break: break-all;\">" + 
+				"<div class=\"row\"><div class=\"col-md-4 minimizable\"><div>abi:</div><div style=\"color:blue;\">[expand]</div></div>" + 
+				"<div class=\"col-md-8 white-gradient minimize\"><textarea rows=\"5\" style=\"width: 100%;\" id=\"abi" + c + "\">" + 
 				JSON.stringify(abi) + 
-				"</div></div>" + 
-				"<div class=\"row\"><div class=\"col-md-4\">code: </div>" + 
-				"<div class=\"col-md-8\" style=\"word-break: break-all;\">" + 
+				"</textarea></div></div>" + 
+				"<div class=\"row\"><div class=\"col-md-4 minimizable\"><div><hr />code:</div><div style=\"color:blue;\">[expand]</div></div>" + 
+				"<div class=\"col-md-8 white-gradient minimize\"><hr /><textarea rows=\"5\" style=\"width: 100%;\" id=\"code" + c + "\">" + 
 				evmCode + 
-				"</div></div>" +
-				"<div class=\"row\"><div class=\"col-md-4\">address: </div>" + 
-				"<div id=\"contractAddress" + c + "\" class=\"col-md-8\" style=\"word-break: break-all;\">[contract not deployed]</div></div>");
+				"</textarea></div></div>" + 
+				"<div class=\"row\"><div class=\"col-md-4\"><div><hr />address:</div></div>" + 
+				"<div id=\"contractAddress" + c + "\" class=\"col-md-8\" style=\"word-break: break-all;\"><hr />[contract not deployed]</div></div>");
 				c = c + 1;
 			}
+			
+			$(".minimizable").click(function(){
+				// clicking on an expandable item toggles it
+				var nxt = $(this).next();
+				var children = $(this).children();
+				if ($(children[1]).text() == "[collapse]")
+				{
+					$(nxt).addClass("white-gradient");
+					$(nxt).addClass("minimize");
+					$(children[1]).text("[expand]");
+				}
+				else
+				{
+					$(nxt).removeClass("white-gradient");
+					$(nxt).removeClass("minimize");
+					$(children[1]).text("[collapse]")
+				}
+
+			});
 		}
 		catch (e)
 		{
@@ -267,14 +315,20 @@ $(function(){
 	})
 	
 	$("#buttonDeploy").click(function(event){
+		// clicking "deploy" creates the contract on the blockchain
 		try
 		{
 			var primaryAddress = web3.eth.accounts[0];
 			
 			for (var c = 0; c < contracts.length; c++)
 			{
-				var MyContract = web3.eth.contract(contracts[c].abi);
-				contracts[c].contract = MyContract.new({from: primaryAddress, data: contracts[c].code})
+				// todo: read abi and code from text areas
+				var abi = JSON.parse($("#abi" + c).val());
+				var code = $("#code" + c).val();
+				console.log(abi);
+				console.log(code);
+				var MyContract = web3.eth.contract(abi);
+				contracts[c].contract = MyContract.new({from: primaryAddress, data: code, gas: 1000000})
 				$("#contractAddress" + c).text("[waiting for contract to be mined...]");
 				contracts[c].status = 2;
 			}
@@ -285,16 +339,17 @@ $(function(){
 		}
 	})
 	
+	// configuring the solidity (actually JS) editor
 	editor = ace.edit("editor");
     editor.setTheme("ace/theme/twilight");
     editor.session.setMode("ace/mode/javascript");
 	editor.getSession().setUseWorker(false);	// disable syntax checker since we just use the javascript version which does not know contracts and other solidity-only features
 	
-	setInterval(updateLastBlockCounter, 2000);
+	setInterval(updateLastBlockCounter, 2000);	// updates UI with network info
 });
 
 function updateLastBlockCounter()
-{
+{	// this function is being called periodically to update the UI with network stats
 	try
 	{
 		var blkNo = web3.eth.blockNumber;
@@ -308,29 +363,24 @@ function updateLastBlockCounter()
 		for (var c = 0; c < contracts.length; c++)
 		{	// check all contracts
 			if (contracts[c].status == 1)
-				$("#contractAddress" + c).text("[contract not deployed]");
+				$("#contractAddress" + c).html("<hr />[contract not deployed]");
 			else if (contracts[c].status == 2)
 			{	// if contract has status "deployed" then check if it has been mined, if so, show its address
 				if (typeof contracts[c].contract.address === 'undefined')
-					$("#contractAddress" + c).text("[waiting for contract to be mined]");
+					$("#contractAddress" + c).html("<hr />[waiting for contract to be mined]");
 				else
 				{
 					contracts[c].status = 3;
-					$("#contractAddress" + c).text(contracts[c].contract.address);
+					$("#contractAddress" + c).html("<hr />" + contracts[c].contract.address);
 				}
 			}
 			else if (contracts[c].status == 3)
-				$("#contractAddress" + c).text(contracts[c].contract.address);
+				$("#contractAddress" + c).html("<hr />" + contracts[c].contract.address);
 			else if (contracts[c].status == 4)
-				$("#contractAddress" + c).text("[error]");
+				$("#contractAddress" + c).html("<hr />[error]");
 			else
-				$("#contractAddress" + c).text("[unknown contract status id: " + contracts[c].status + "]");
+				$("#contractAddress" + c).html("<hr />[unknown contract status id: " + contracts[c].status + "]");
 		}
-
-		if (typeof contract === 'undefined')
-			$("#contractAddress").text("[contract not deployed]");
-		else
-			$("#contractAddress").text(contract.address);
 	}
 	catch (e)
 	{
@@ -368,7 +418,7 @@ function reloadContentFromAddress(target, address)
 }
 
 function updateGethAddress()
-{
+{	// reads the geth address from the settings tab and creates a web3 object which connects to this address
     var gethAddress = $("#gethAddress").val();
     web3 = new Web3(new Web3.providers.HttpProvider(gethAddress));
 	info("set geth node address to: " + gethAddress);
@@ -378,9 +428,23 @@ function updateGethAddress()
 	{
 		info("latest block number: " + web3.eth.blockNumber);
 		
-		//web3.eth.defaultAccount = defaultAccount;
-		//instanceContReg = web3.eth.contract(contRegAbi).at(contRegContractAddress);
-		//instanceNameReg = web3.eth.contract(nameRegAbi).at(nameRegContractAddress);
+		// set default account
+		web3.eth.defaultAccount = web3.eth.accounts[0];
+		defaultAccount = web3.eth.accounts[0];
+		// read abi definition and address for both contracts (name registry and content registry) from UI and use values to create contract object from them
+		var contRegAbi = JSON.parse($("#contRegAbi").val());
+		var nameRegAbi = JSON.parse($("#nameRegAbi").val());
+		var contRegContractAddress = $("#contRegAddress").val();
+		var nameRegContractAddress = $("#nameRegAddress").val();
+		instanceContReg = web3.eth.contract(contRegAbi).at(contRegContractAddress);
+		instanceNameReg = web3.eth.contract(nameRegAbi).at(nameRegContractAddress);
+		
+		/* contract instances can now be used as follows:
+			- const functions with return value
+			var num = instanceContReg.getNumberOfEntries(web3.eth.accounts[0]).c[0]
+			
+			- non-const functions with return value (async):
+			*/
 	}
 	catch (e)
 	{
@@ -398,7 +462,7 @@ function contrHolder (source, abi, code)
 }
 
 function updateIpfsAddress()
-{
+{	// the IPFS node address is read from the settings tab, then the ipfs object is (re-)created with this endpoint
     var ipfsAddress = $("#ipfsAddress").val();
 	ipfs = ipfsAPI(ipfsAddress);
     info("Set ipfs node address to: " + ipfsAddress);
@@ -416,9 +480,7 @@ function info(msg)
 {	// displays an info message in browser console and in debug tab
 	console.log("Info: " + msg)
 	
-	// todo: we might want to pop up if we enable debug mode or increased verbosity
-			// but then it should be an info and not alert class
-			
+	// todo: we might want to pop up if we enable debug mode or increased verbosity			
 	//$(".container").prepend("<div class='alert alert-info alert-dismissible' role='alert'><button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button><strong>Info:</strong><br />" + msg + "</div>");
 	$("#debug").append(document.createTextNode("Info: " + msg));
 	$("#debug").append("<br />");
